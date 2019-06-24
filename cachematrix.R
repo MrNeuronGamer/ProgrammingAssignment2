0aa8a9020a824fba20a8f2531e3e943e0b86d443
MrNeuronGamer <khoroshilov.dmitry@mail.ru>
2019-06-24 21:11:33 +0300

    one

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invrs <- NULL
+  set <- function(y) {
+    x <<- y
+    invrs <<- NULL 
+  }
+  get <- function() x
+  setinvrs<- function(z) invrs <<- z
+  getinvrs <- function() invrs
+  list(set = set, get = get, setinvrs = setinvrs, getinvrs = getinvrs)
+  
+  ## This function creates special data type namely CacheMatrix that contains matrix itself and its inverse.
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invrs <- x$getinvrs()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  data <- x$get()
+  invrs <- solve(data, ...)
+  x$setinvrs(invrs)
+  invrs
+  
+## This function checks out whether an object passed into it has already had its inverse inside, 
+## if not -- determines its inverse and writes it into the object. 
 }
+
